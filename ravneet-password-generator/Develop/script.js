729edfbacc34d7191d32ff8a828564d684de3d88
Ravneet Kaur Sammewali <ravneet.sammewali01@gmail.com>
2022-10-29 07:14:23 -0400

    Main Idea

--- ravneet-password-generator/Develop/script.js
+++ ravneet-password-generator/Develop/script.js
@@ -3,11 +3,20 @@ var generateBtn = document.querySelector("#generate");
 
 // Write password to the #password input
 function writePassword() {
-  var password = generatePassword();
-  var passwordText = document.querySelector("#password");
+  //var password = generatePassword();
+  //var passwordText = document.querySelector("#password");
+
+  var passwordlength = prompt("Length of the password?");
+
+  function showResponse(passwordlength){
+  passwordlength.preventDefault();
+  if (isNaN(passwordlength)){   
+    console.log("Please enter a integer!");
+  }}
+
 
   
-  passwordText.value = password;
+ 
 
 }
 
