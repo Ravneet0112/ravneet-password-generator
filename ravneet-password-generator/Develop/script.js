8f981e1cb172c489c8ea7e2c3bfef27334b8add4
Ravneet Kaur Sammewali <ravneet.sammewali01@gmail.com>
2022-10-31 00:37:59 -0400

    Used confirm funtion in JS

--- ravneet-password-generator/Develop/script.js
+++ ravneet-password-generator/Develop/script.js
@@ -1,10 +1,7 @@
 // Assignment Code
 var generateBtn = document.querySelector("#generate");
 
-
-
-
-
+function generatePassword() {
   console.log("You clicked the button");
 
   var length = prompt("Length of the password?");
@@ -15,29 +12,62 @@ var generateBtn = document.querySelector("#generate");
 
 
   if (parseInt(length) < 8 || parseInt(length) > 128) {
-    alert ("Please enter number more than 8 or less than 128");
+    alert ("Please enter number more than 8 or less than 128");   
+    return; 
+  }
+ 
+  for (var i =0; i<parseInt(length); i++){
+    var lower = confirm("Do you want to include lower case");
+    if(lower === true) {
+      var lowercase = console.log(getRandomLower());
+      }
+  
+    var upper = confirm("Do you want to include uppercase");
+    if (upper === true) {
+      var uppercase = console.log(getRandomUpper()); 
+       }
+  
+    var symbol = confirm("Do you want to include symbol");
+    if (symbol === true) {
+      var symbolcase = console.log(getRandomSymbol()); 
+      
+    }
     
+    var number = confirm("Do you want to include number");
+    if (number === true) {
+      var numbercase = console.log(getRandomNum()); }
+
+
+   //function getresult(lowercase, uppercase, symbolcase, numbercase) {}
+      let generatedPassword = '';
+
+      const typesCount = lower + upper + number + symbol;
+
+      const typeArr = [ lower , upper , number , symbol ]
+      .filter(item => Object.values(item)[1]);
+      console.log('typeArr ', typeArr);
+
+      if (typesCount === 0){
+        return '';
+      
+      }
   }
 
-
-  var lower = prompt("Do you want to include lower case");
-  var upper = prompt("Do you want to include uppercase");
-  var symbol = prompt("Do you want to include symbol");
-  var number = prompt("Do you want to include number");
- 
-  function generatePassword(lowercase, uppercase, symbol, number){
-
-  
 }
+ 
 
-const randomFunc = {
-  lowercase: getRandomLower,
+
+/*const randomFunc = {
+  lower: getRandomLower(),
   /*upper: getRandomUpper,
   number: getRandomNum,
-  symbol: getRandomSymbol,*/
-};
+  symbol: getRandomSymbol,}*/
 
-console.log(lowercase);
+
+
+
+
+
 
 
 // const lowecaserEL = document.getElementById('lowercase');
@@ -53,17 +83,7 @@ function writePassword() {
   var password = generatePassword();
   var passwordText = document.querySelector("#password");
 
-  
 
-
-  
-
-
- /* function showResponse(passwordlength){
-  passwordlength.preventDefault();
-  if (isNaN(passwordlength)){   
-    console.log("Please enter a integer!");
-  }}*/
 }
 
 
@@ -71,23 +91,25 @@ function writePassword() {
     //gets lowercase # from the char code 97-123
     // Math.random is a decimal value to change it to a whole number
     // Math.floor is used
-    return String.fromCharCode(Math.floor(Math.random)* 26 + 97);
+    return String.fromCharCode(Math.floor(Math.random()* 26) + 97);
+
   }
+  
 
   function getRandomUpper(){
     //Upper case letters start from 65 to 96
-    return String.fromCharCode(Math.floor(Math.random)* 26 + 65);
+    return String.fromCharCode(Math.floor(Math.random()* 26) + 65);
   }
 
   function getRandomNum(){
     //Numbers starts from 48
-    return String.fromCharCode(Math.floor(Math.random)* 26 + 65);
+    return String.fromCharCode(Math.floor(Math.random()* 10) + 48);
   }
 
   function getRandomSymbol(){
     //Upper case letters start from 65 to 96
     const symbols = '!@#$%^&*()_+{}[]<>?|/=-,.';
-    return symbols[Math.floor(Math.random() * symbols.lenght)];
+    return symbols[Math.floor(Math.random() * symbols.length)];
   }
 
   
